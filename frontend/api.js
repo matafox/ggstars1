ea858c906e81354529be3a8351fea5c03f7603d8
paulmatafox <haqintosh@gmail.com>
2025-04-26 21:06:17 +0200

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,14 +1,16 @@
 const PANDA_SCORE_API_TOKEN = "zsH2ngzJ0h2pQ0tu3PukEU_5Yp9ERc3lJgfWbdmnjsMqi8jIpJ0";
-const PANDA_SCORE_API_URL = "https://api.pandascore.co/csgo/matches/upcoming";
 
-async function getMatches() {
-  const response = await fetch(PANDA_SCORE_API_URL, {
+export async function getLiveMatches() {
+  const response = await fetch("https://api.pandascore.co/csgo/matches/upcoming", {
     headers: {
-      Authorization: Bearer ${PANDA_SCORE_API_TOKEN}
-    }
+      Authorization: Bearer ${PANDA_SCORE_API_TOKEN},
+    },
   });
-  const matches = await response.json();
-  return matches;
-}
 
-export { getMatches };
+  const allMatches = await response.json();
+  return allMatches.map(m => ({
+    id: m.id,
+    teams: m.opponents[0]?.opponent?.name + ' vs ' + m.opponents[1]?.opponent?.name,
+    status: m.status === 'running' ? 'LIVE' : new Date(m.begin_at).toLocaleTimeString()
+  }));
+}
