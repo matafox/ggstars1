272a9d199aece2f08f00b852815cd4b49b036c85
paulmatafox <haqintosh@gmail.com>
2025-04-27 23:31:23 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
