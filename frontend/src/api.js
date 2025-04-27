17bf42aee51043eae1f18fcc04064d23be3aeb5f
paulmatafox <haqintosh@gmail.com>
2025-04-27 14:20:28 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ export async function getMatches() {
   try {
     const response = await fetch('https://ggstars.onrender.com/api/matches');
     const data = await response.json();
-    return data.matches || [];
+    return data || []; // Просто повертаємо data
   } catch (error) {
     console.error('Error fetching matches:', error);
     return [];
