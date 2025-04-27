503129bf60202f70bb471ad4837bd797d1dc05eb
paulmatafox <haqintosh@gmail.com>
2025-04-27 21:18:33 +0200

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,16 +1,22 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+
   const keyboard = {
     reply_markup: {
       inline_keyboard: [
         [
           {
             text: '🚀 Відкрити GGStars',
-            web_app: { url: 'https://jovial-alpaca-bcc007.netlify.app/' }
+            web_app: { url: 'https://jovial-alpaca-bcc007.netlify.app/' } // заміни на свій домен!
           }
         ]
       ]
     }
   };
+
   bot.sendMessage(chatId, 'Ласкаво просимо в GGStars!', keyboard);
 });
